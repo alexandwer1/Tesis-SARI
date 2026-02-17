8ca6d9bb845973af857b702d20b20fade9200d96
alexandwer1 <121064461+alexandwer1@users.noreply.github.com>
2026-02-17 13:15:45 -0600

    Update SARI.py

--- SARI.py
+++ SARI.py
@@ -116,7 +116,7 @@ def get_conversational_chain():
 def user_input(user_question):
     # 1. Configuramos los embeddings con la clave de Streamlit
     api_key = st.secrets["GOOGLE_API_KEY"]
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004",google_api_key=st.secrets["GOOGLE_API_KEY"])
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-001",google_api_key=st.secrets["GOOGLE_API_KEY"])
     print(">>> Cargando base de datos vectorial...") 
     new_db = FAISS.load_local(
         "faiss_index", 
@@ -178,6 +178,7 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
 
 
