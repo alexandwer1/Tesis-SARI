0e45895d2d742974207460c02c2f34cfa2e2915a
alexandwer1 <121064461+alexandwer1@users.noreply.github.com>
2026-02-18 10:10:34 -0600

    Update SARI.py

--- SARI.py
+++ SARI.py
@@ -6,7 +6,7 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
-from langchain.chains.question_answering import load_qa_chain # Corrección de importación
+from langchain.chains import load_qa_chain
 import time
 import os
 
@@ -136,3 +136,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
